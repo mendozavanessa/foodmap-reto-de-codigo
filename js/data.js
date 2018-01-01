77e75720be3bbb6f2883ebff9f883a1253b82311
vaneshita <144995@unsaac.edu.pe>
2018-01-01 11:39:29 -0500

    añadiendo funcionalida en busqueda de restaurant y platos de comida

---
+++ js/data.js
@@ -0,0 +1,74 @@
+var data = {
+  'Comas': {
+    'Paladar': {
+      'ceviche':{
+        'photo': 'assets/img/ceviche.jpg'
+      },
+      'mariscos': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
+      },
+    },
+    'Tiburon': {
+      'tiradito':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'jalea': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
+      },
+    },
+  },
+  'Breña': {
+    'Cholita': {
+      'olluquito':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'caucau': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+    'Costillitas': {
+      'costillar':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'anticuchos': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+  },
+  'Miraflores': {
+    'Antojitos': {
+      'ommelet':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'milanesa': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+    'Tentaciones': {
+      'chuletaAsada':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'salpicon': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+  },
+  'Lince': {
+    'PizzaHut': {
+      'peperoni':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'huwaiana': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+    'Fogon': {
+      'chicharron':{
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+      'parrillas': {
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+      },
+    },
+  },
+};
--- js/vistaPrincipal.js
+++ js/vistaPrincipal.js
@@ -1,1 +1,35 @@
-
+$(document).ready(function(){
+  var distrito = $('#input-distrito');
+  var ubicacion = $('#ubicacion');
+  var primero = $('#primero');
+  var segundo = $('#segundo');
+  var img1 = $('#img1');
+  var img2 = $('#img2');
+  var img3 = $('#img3');
+  var img4 = $('#img4');
+  var datos= Object.keys(data);
+  console.log(datos);
+  distrito.keyup(function(e) {
+    for (var i = 0; i < datos.length; i++) {
+      if (distrito.val() === datos[i]) {
+        if(e.keyCode == 13) {
+          $('#ubicacion').text('restaurantes en'+' '+datos[i]);
+          var restaurant = Object.keys(data[datos[i]]);
+          var comidas1 = Object.keys(data[datos[i]][restaurant[0]]);
+          var comidas2 = Object.keys(data[datos[i]][restaurant[1]]);
+          console.log(comidas1[0]);
+          $('#primero').text(restaurant[0]);
+          primero.on('click', function(event) {
+            img1.attr('src', '../assets/img/' + comidas1[0] + '.jpg');
+            img2.attr('src', '../assets/img/' + comidas1[1] + '.jpg');
+          });
+          $('#segundo').text(restaurant[1]);
+          segundo.on('click', function(event) {
+            img3.attr('src', '../assets/img/' + comidas2[0] + '.jpg');
+            img4.attr('src', '../assets/img/' + comidas2[1] + '.jpg');
+        });
+          }
+        }
+      }
+    });
+});
